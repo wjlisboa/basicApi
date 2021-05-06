3e50bc5f828b797bd2ddaeb34354ba504fce2160
Welington Lisboa <wjlisboa@gmail.com>
2021-05-06 15:31:55 -0300

    Inicial commit

---
+++ src/test/java/com/basicApi/BasicApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.basicApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
